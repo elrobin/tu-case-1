f7010e5e6df8819efb7347fa579d95a7d19dc197
elrobin <elrobinster@gmail.com>
2019-08-15 13:37:50 +0200

    preparing data for ISSI poster

---
+++ code/poster_figs.R
@@ -0,0 +1,9 @@
+# Polar chart example for SSCI using this code:
+# https://medium.com/optima-blog/using-polar-coordinates-for-better-visualization-1d337b6c9dec
+
+# Variables can be the following:
+  # - OA share is Open practices
+  # - Capacity building: funded pubs, number of collabs in first year
+  # - Scientific engagement: number of collaborators, share of new collaborations
+# - Social engagement: mentions in policy, mentions in news media
+# - Trajectory: years of international experience; int collab
